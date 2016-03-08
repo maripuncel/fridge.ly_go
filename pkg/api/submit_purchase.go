76391e903dcb9211f4c6c246896a8363f2b5e4f0
Michael Puncel <mpuncel@squareup.com>
2016-03-07 22:12:57 -0800

    Add POST /api/basket endpoint
    This creates a "basket" by iterating over the items in the basket and
    adding them to the purchased_items table. This occurs in a transaction
    so either all of none of the items are added.
    
    There is currently no baskets table, but one should be added so that a
    list of items may be linked back to the basket they were a part of.

---
+++ pkg/api/submit_purchase.go
@@ -0,0 +1,69 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+// Note: the "binding" tags here don't actually work, probably because
+// they're nested
+type PurchasedItem struct {
+	Name     string   `json:"name" binding:"required"`
+	Quantity int      `json:"quantity" binding:"required"`
+	Unit     string   `json:"unit" binding:"required"`
+	Cost     *float64 `json:"cost" binding:"required"`
+}
+
+type CreateBasketRequest struct {
+	PurchasedItems []PurchasedItem `json:"purchased_items" binding:"required"`
+}
+
+type CreateBasketResponse struct {
+	OK bool `json:"ok"`
+}
+
+func (api API) CreateBasketHandler(c *gin.Context) {
+	var submitPurchaseRequest CreateBasketRequest
+	if c.BindJSON(&submitPurchaseRequest) == nil {
+
+		tx, err := api.DB.Begin()
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+		}
+
+		if !c.IsAborted() {
+			purchasedItemsTableName := "purchased_items"
+			for _, purchasedItem := range submitPurchaseRequest.PurchasedItems {
+				_, err := tx.Exec(
+					fmt.Sprintf("INSERT INTO %s (name, quantity, unit, cost) VALUES ($1, $2, $3, $4)", pq.QuoteIdentifier(purchasedItemsTableName)),
+					purchasedItem.Name,
+					purchasedItem.Quantity,
+					purchasedItem.Unit,
+					purchasedItem.Cost,
+				)
+				if err != nil {
+					c.AbortWithError(http.StatusInternalServerError, err)
+					err := tx.Rollback()
+					if err != nil {
+						c.Error(err)
+					}
+					break
+				}
+			}
+		}
+
+		if !c.IsAborted() {
+			err := tx.Commit()
+			if err != nil {
+				c.AbortWithError(http.StatusInternalServerError, err)
+			}
+		}
+
+		var resp CreateBasketResponse
+		resp.OK = !c.IsAborted()
+		c.JSON(http.StatusOK, resp)
+	}
+}
